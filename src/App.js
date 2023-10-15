9873f1432e01a5bc1527df7bd35fa9d006b5aa43
Anirudha Patil <aniruddhap66@gmail.com>
2023-10-15 10:33:51 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [selectedYear, setSelectedYear] = useState(null);
+  const [age, setAge] = useState(0);
+
+  const handleYearChange = (date) => {
+    setSelectedYear(date);
+  };
+
+  const calculateAge = () => {
+    if (selectedYear) {
+      const currentYear = new Date().getFullYear();
+      const enteredYear = selectedYear.getFullYear();
+
+      if (!isNaN(enteredYear) && enteredYear <= currentYear && enteredYear > 0) {
+        const age = currentYear - enteredYear;
+        setAge(age);
+      } else {
+        alert("Please enter a valid year.");
+      }
+    } else {
+      alert("Please select a year.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Age Calculator</h1>
+      <div className="input-container">
+        <DatePicker
+          selected={selectedYear}
+          onChange={handleYearChange}
+          showYearPicker
+          dateFormat="yyyy"
+          isClearable // Enable clearing the year selection
+        />
+        <button onClick={calculateAge}>Calculate</button>
+      </div>
+      <div className="result">{age > 0 && `Your age is ${age} years.`}</div>
     </div>
   );
-}
+};
 
 export default App;
